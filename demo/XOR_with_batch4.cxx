d36fbdcbadc70166552d4f7497ab35d5b6bbad25
durandnico <nicod4956@gmail.com>
2024-04-24 01:18:53 -0400

    demo de XOR avec des batch de 4 (juste pour flex et montrer que ca marche)

---
+++ demo/XOR_with_batch4.cxx
@@ -0,0 +1,109 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "global.hxx"
+#include "Dense.hxx"
+#include "errors_functions.hxx"
+#include "Sigmoid.hxx"
+#include "Tanh.hxx"
+#include "Relu.hxx"
+#include "LeakyRelu.hxx"
+#include "Optimizer.hxx"
+
+constexpr double learning_rate = 0.01;
+constexpr int epochs = 500000;
+constexpr double alpha = 0.01;
+
+int main()
+{
+
+  srand(time(NULL));
+
+   /* creating neural network for XOR w/ batch of size 4*/
+  NeuralNetwork::Dense* NN[] = {
+    new NeuralNetwork::Dense(2, 3, new NeuralNetwork::Adam{}),
+    new NeuralNetwork::LeakyRelu{alpha},
+    new NeuralNetwork::Dense(3, 1, new NeuralNetwork::Adam{}),
+    new NeuralNetwork::Sigmoid{}
+  };
+
+
+  /* training data */
+  Eigen::MatrixXd X(4, 2);
+  X << 0, 0,
+       0, 1,
+       1, 0,
+       1, 1;
+
+
+  Eigen::VectorXd y(4);
+  y << 0, 1, 1, 0;
+
+  /* training */
+  std::vector<double> losses;
+  losses.reserve(epochs);
+  for (int i = 0; i < epochs; i++)
+  {
+    double loss = 0;
+
+    {
+      Eigen::MatrixXd inputs = X;
+      Eigen::MatrixXd target = y;
+      
+      /* forward */
+      for (auto layer : NN)
+      {
+        inputs = layer->forward(inputs);
+      }
+
+      
+      /* loss */
+      const VectorXd tmp = NeuralNetwork::mean_squared_error(inputs, target);
+      loss += tmp.sum();
+      std::cout << "loss: " << loss << std::endl;
+
+      /* backward */
+      Eigen::MatrixXd grad = NeuralNetwork::mean_squared_error_prime(target, inputs);
+      
+      /* loop from last to first*/
+      for(int i = 3; i >= 0; i--)
+      {
+        grad = NN[i]->backward(grad);
+      }
+    }
+
+    loss /= 4;
+    losses.emplace_back(loss);
+  }
+
+  // printf("losses: [ ");
+  // for (double l : losses)
+  // {
+  //   printf("%f, ", l);
+  // }
+  // putchar(']');
+  // putchar('\n');
+
+  /* testing */
+  for (int i = 0; i < 4; i++)
+  {
+    Eigen::VectorXd inputs = X.row(i);
+    Eigen::VectorXd target = y.row(i);
+
+    for (auto layer : NN)
+    {
+      inputs = layer->forward(inputs);
+    }
+
+    printf("target: %f, prediction: %f\n", target(0), inputs(0));
+  }
+
+  for (NeuralNetwork::Dense* layer : NN)
+  {
+    delete layer;
+  }  
+
+  return 0;
+}
